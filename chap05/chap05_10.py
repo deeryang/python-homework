4c2e10aa24699da0b2832a5cb1c56825363cbaac
deeryang <745898859@qq.com>
2019-01-10 22:26:00 +0800

    update comment

--- chap05/chap05_10.py
+++ chap05/chap05_10.py
@@ -6,12 +6,19 @@ Created on Thu Jan 10 21:52:10 2019
 @author: yang
 """
 
+# =============================================================================
+# read the data from in.txt
+# use the console window
+# command: python3 chap05_10.py < in.txt
+# =============================================================================
 numberOfStudents = eval(input("Enter the number of students: "))
+print()
 
 maxScore = 0
 
 for i in range(numberOfStudents):
     score = eval(input("Enter the score: "))
+    print()
     maxScore = max(score, maxScore)
     
 print("The maximum score is:", maxScore)
