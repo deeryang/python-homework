a1d5101592c4600716b9fc004410574d75ac42da
deeryang <745898859@qq.com>
2019-01-13 14:10:36 +0800

    update

--- chap05/chap05_09.py
+++ chap05/chap05_09.py
@@ -9,8 +9,11 @@ Created on Thu Jan 10 21:40:22 2019
 cost = 10000
 total = 0
 for i in range(10):
+    cost = cost * (1 + 0.05)
+    
+for i in range(4):
     total += cost
     cost = cost * (1 + 0.05)
 
 print("The cost of ten years later: " + format(cost, ".2f"))
-print("The total cost of ten years: " + format(total, ".2f"))
+print("The total cost of four years ten years later: " + format(total, ".2f"))
